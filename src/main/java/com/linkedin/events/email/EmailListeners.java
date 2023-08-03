e07257996fc1b54feabb7063999873f688fbdae5
Sayantan Chakraborty <sayantan.chakraborty@nagarro.com>
2023-08-03 16:10:50 +0530

    Initial Commit

---
+++ src/main/java/com/linkedin/events/email/EmailListeners.java
@@ -0,0 +1,34 @@
+package com.linkedin.events.email;
+
+import com.linkedin.events.customer.CustomerRegisteredEvent;
+import com.linkedin.events.customer.CustomerRemovedEvent;
+import com.linkedin.events.order.OrderCompletedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class EmailListeners
+{
+    private final EmailService emailService;
+
+    @EventListener
+    public void onRegisterEvent(CustomerRegisteredEvent event)
+    {
+        emailService.sendRegisterEmail(event.getCustomer());
+    }
+
+    @EventListener
+    public void onCustomerRemovedEvent(CustomerRemovedEvent event)
+    {
+        emailService.sendCustomerRemovedEmail(event.getCustomer());
+    }
+    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onOrderCompleted(OrderCompletedEvent event){
+        emailService.sendOrderEmail(event.getOrder());
+    }
+}
---
+++ src/main/java/com/linkedin/events/promotion/PromotionListeners.java
@@ -0,0 +1,22 @@
+package com.linkedin.events.promotion;
+
+import com.linkedin.events.customer.CustomerRegisteredEvent;
+import com.linkedin.events.order.OrderCompletedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PromotionListeners {
+
+    private final PromotionService promotionService;
+    @EventListener(condition = "#event.customer.newsletter")
+    public void onRegisteredEvent(CustomerRegisteredEvent event){
+        promotionService.applyPromotion(event.getCustomer());
+    }
+    @EventListener
+    public void onOrderCompleted(OrderCompletedEvent event){
+        promotionService.calculateRewardPoints(event.getOrder());
+    }
+}
